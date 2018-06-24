2383903b2aa54ed85a099178540e7ff14c1d7c47
Christian <bouwensec@gmail.com>
2018-06-24 00:06:50 -0400

    Added some art assets.

--- Assets/SpinnyBoi.cs
+++ Assets/SpinnyBoi.cs
@@ -6,7 +6,7 @@ public class SpinnyBoi : MonoBehaviour {
 
 	void Update()
     {
-        GetComponent<Rigidbody2D>().angularVelocity = -25;
+        GetComponent<Rigidbody2D>().angularVelocity = -20;
     }
 
 }
